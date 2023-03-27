877f2189f8b1929f51b0326090ecda79228ea47c
z-seni <35400507+z-seni@users.noreply.github.com>
2023-03-27 08:00:51 +0200

    added reading flag and reading to and from .txt file

--- V2/V2/Program.cs
+++ V2/V2/Program.cs
@@ -1,1 +1,47 @@
-﻿
+﻿using System.ComponentModel;
+using System.Security.Cryptography.X509Certificates;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        string input = args[1];
+        string numbers = File.ReadAllText(input);
+        string[] array = numbers.Split(' ');
+        int length = array.Length;
+        int[] A = new int[length];
+
+
+        if (args[0] == "0")
+        {
+            
+            //Pisanje v izhodno datoteko
+            using (StreamWriter writer = new StreamWriter("out.txt"))
+            {
+                foreach (var item in B)
+                {
+                    writer.Write(item);
+                    writer.Write(' ');
+                }
+            }
+        }
+        else if (args[0] == "1")
+        {
+
+            //Pisanje v izhodno datoteko
+            using (StreamWriter writer = new StreamWriter("out.txt"))
+            {
+                foreach (var item in B)
+                {
+                    writer.Write(item);
+                    writer.Write(' ');
+                }
+            }
+        }
+        else
+        {
+            Console.WriteLine("Napačana zastavica");
+        }
+    }
+}
